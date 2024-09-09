17bc6722a792391be27840bcf65c75ff610939f6
8auti <bautistasilvalosilla@gmail.com>
2024-09-09 15:32:09 -0300

    Imprimir Funciones

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ import javax.swing.JOptionPane;
 public class Main {
 	public static void main(String[] args) {
 		Trabajador miAmigo = new Trabajador("Jorge Carlos", LocalDate.of(2020, 3, 16), 100000.0);
-		JOptionPane.showMessageDialog(null, miAmigo.calcularDiasVacaciones());
+		JOptionPane.showMessageDialog(null, "Dias de vacaciones: "+miAmigo.calcularDiasVacaciones());
+		JOptionPane.showMessageDialog(null, "Aumento Salarial: "+miAmigo.calcularAumentoSalarial());
+		JOptionPane.showMessageDialog(null, "Indemnizacion de Despido: "+miAmigo.calcularIndemnizacionDespido());
 	}
 }
